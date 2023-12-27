bc90948e25ed5c6abddf42fcb3d77181a4dfb37a
unknown <syednew5000@gmail.com>
2023-12-27 19:13:38 +0600

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,8 @@
 import Nav from "@/components/Nav/Nav";
 import Home from "./home/page";
 import Footer from "@/components/Footer/Footer";
-import { authOptions } from "@/lib/AuthOptions";
-import { getServerSession } from "next-auth/next";
-import { useSession } from "next-auth/react";
 const HomePage = async() => {
-  // const session = await getServerSession(authOptions);
-  // console.log({session});
+  
 
   return (
     <>
